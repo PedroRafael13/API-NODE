26c519e2bd59687a1f27ce4795ca6fb271e48094
PedroRafael13 <pedrorafaeldsn12@gmail.com>
2025-03-31 21:06:25 -0400

    explore concert of routes

--- server.js
+++ server.js
@@ -2,4 +2,14 @@ import express from "express";
 
 const App = express()
 
+App.get("/", (req, res) => {
+  res.send("Hello word")
+})
+
+
+// TODO : TREINANDO CONCEITO DE ROTAS NO NODE.JS
+App.get("/infor", (req, res) => {
+  res.send("somente pessoas autorizada podem entrar")
+})
+
 App.listen(3000, console.log("The server is runnering at port 3000"))
